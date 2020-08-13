ecdd44e0c529f04eac711272eb4d355e718be31b
mountAP <ap44122@gmail.com>
2020-08-13 13:23:18 +0530

    added spokes and circles

--- ferris-wheel-matterJS/script.js
+++ ferris-wheel-matterJS/script.js
@@ -1,12 +1,12 @@
-// import "./lib/matter.js";
-
 const html = document.querySelector('html');
 
 // module aliases
 let Engine = Matter.Engine,
   Render = Matter.Render,
   World = Matter.World,
+  Body = Matter.Body,
   Bodies = Matter.Bodies,
+  Composite = Matter.Composite,
   Constraint = Matter.Constraint,
   Mouse = Matter.Mouse,
   MouseConstraint = Matter.MouseConstraint;
@@ -20,7 +20,8 @@ let render = Render.create({
   engine: engine,
   options: {
     width: window.innerWidth,
-    height: window.innerHeight
+    height: window.innerHeight,
+    showConvexHulls: true
   }
 });
 
@@ -59,32 +60,76 @@ function Ground(center_vw, elevation_vh, span_vw, thickness_vh) {
 function FerrisWheel(x_vw, y_vh) {
   this.x = vwTOpx(x_vw);
   this.y = vhTOpx(y_vh);
-  this.radius = vminTOpx(30);
-  this.body = Bodies.polygon(this.x, this.y, 8, this.radius, { torque: 1000 });
+  this.radius = vminTOpx(36);
+  this.parts = [];
 
+  let frame = Bodies.polygon(this.x, this.y, 12, this.radius);
+
+  // circles
+  this.parts.push(Bodies.circle(this.x, this.y, vminTOpx(34), { density: 0 }));
+  this.parts.push(Bodies.circle(this.x, this.y, vminTOpx(10), { density: 0 }));
+
+  // junctions
+  for (let vertex of frame.vertices) {
+    this.parts.push(Bodies.circle(vertex.x, vertex.y, vminTOpx(0.4), { label: 'Junction' }));
+  }
+
+  this.body = Body.create({ parts: this.parts });
   World.add(engine.world, this.body);
+
+  // spokes
+  for (let part of this.parts) {
+    if (part.label === 'Junction') {
+      World.add(engine.world, Constraint.create({
+        bodyA: this.body,
+        bodyB: part,
+        render: {
+          anchors: false,
+          lineWidth: 0.1
+        }
+      }));
+    }
+  }
 }
 
 function Support(wheel, base) {
   this.wheel = wheel;
   this.base = base;
 
+  let render = {
+    lineWidth: 5,
+    strokeStyle: 'rgba(255, 255, 255, 0.3)'
+  };
+
   this.left = Constraint.create({
     pointA: { x: this.base.center - vwTOpx(10), y: this.base.elevation },
-    bodyB: this.wheel.body
+    bodyB: this.wheel.body,
+    render
   });
+
 
   this.right = Constraint.create({
     pointA: { x: this.base.center + vwTOpx(10), y: this.base.elevation },
-    bodyB: this.wheel.body
+    bodyB: this.wheel.body,
+    render
   });
+
 
   World.add(engine.world, [this.left, this.right]);
 }
 
-const ferrisWheel = new FerrisWheel(50, 50);
+
+const ferrisWheel = new FerrisWheel(50, 45);
 const ground = new Ground(50, 55 + pxTOvh(ferrisWheel.radius), 44, 1);
 const support = new Support(ferrisWheel, ground);
+
+setInterval(() => {
+  ferrisWheel.body.torque = 0.000001;
+}, 100);
+
+function Human() {
+
+}
 
 
 // run the engine
@@ -92,3 +137,5 @@ Engine.run(engine);
 
 // run the renderer
 Render.run(render);
+
+window.onresize = () => document.location.reload();
