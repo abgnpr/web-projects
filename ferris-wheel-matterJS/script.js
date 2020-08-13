f10123eda6a1f0b2f3f5242c842f0cdc0f5e96aa
mountAP <ap44122@gmail.com>
2020-08-13 23:41:51 +0530

    Added baskets: cleanup required

--- ferris-wheel-matterJS/script.js
+++ ferris-wheel-matterJS/script.js
@@ -1,3 +1,5 @@
+// import "./lib/matter";
+
 const html = document.querySelector('html');
 
 // module aliases
@@ -9,7 +11,9 @@ let Engine = Matter.Engine,
   Composite = Matter.Composite,
   Constraint = Matter.Constraint,
   Mouse = Matter.Mouse,
-  MouseConstraint = Matter.MouseConstraint;
+  MouseConstraint = Matter.MouseConstraint,
+  Vector = Matter.Vector;
+
 
 // create an engine
 let engine = Engine.create();
@@ -29,7 +33,7 @@ let render = Render.create({
 World.add(engine.world, MouseConstraint.create(engine, {
   mouse: Mouse.create(render.canvas),
   constraint: {
-    stiffness: 0.2,
+    stiffness: 1,
     render: {
       visible: false
     }
@@ -43,9 +47,34 @@ const vhTOpx = (h) => (h / 100) * html.clientHeight;
 const vminTOpx = (x) => html.clientHeight < html.clientWidth ? (x / 100) * html.clientHeight : (x / 100) * html.clientWidth;
 const pxTOvh = (x) => x / html.clientHeight * 100;
 const pxTOvw = (x) => x / html.clientWeight * 100;
-
+const degTOrad = (x) => x * Math.PI / 180;
 
 // objects
+
+// categories
+const group = Body.nextGroup(true);
+
+const Template = {
+  seat: function (junction) {
+    this.x = junction.position.x;
+    this.y = junction.position.y;
+    const r = 2.5; // traingle radius
+    const h = r + r / 2; // triangle height
+    const l = 4; // square length
+    const b = r * Math.sqrt(3);// square bredth
+
+    let cap = Bodies.polygon(this.x, this.y, 3, vminTOpx(r), { angle: degTOrad(90) });
+    let basket = Bodies.rectangle(this.x, this.y + vminTOpx(h - r + l / 2), vminTOpx(b), vminTOpx(l));
+    let a = Bodies.circle(this.x, cap.position.y, 0.4);
+    return Body.create({
+      parts: [cap, basket, a],
+      collisionFilter: { group: group },
+      restitution: 0,
+      density: 0.01,
+      frictionAir: 0.05
+    });
+  }
+};
 
 function Ground(center_vw, elevation_vh, span_vw, thickness_vh) {
   this.center = vwTOpx(center_vw);
@@ -54,49 +83,74 @@ function Ground(center_vw, elevation_vh, span_vw, thickness_vh) {
   this.thickness = vhTOpx(thickness_vh);
   this.body = Bodies.rectangle(this.center, this.elevation, this.span, this.thickness, { isStatic: true });
 
-  World.add(engine.world, this.body);
+  this.show = function () { World.add(engine.world, this.body); }
 }
 
 function FerrisWheel(x_vw, y_vh) {
   this.x = vwTOpx(x_vw);
   this.y = vhTOpx(y_vh);
-  this.radius = vminTOpx(36);
-  this.parts = [];
+  this.radius = vminTOpx(40);
+  this.numSeats = 24;
 
-  let frame = Bodies.polygon(this.x, this.y, 12, this.radius);
+  // this.body = Composite.create();
 
-  // circles
-  this.parts.push(Bodies.circle(this.x, this.y, vminTOpx(34), { density: 0 }));
-  this.parts.push(Bodies.circle(this.x, this.y, vminTOpx(10), { density: 0 }));
+  // Composite.add(this.body, )
 
-  // junctions
-  for (let vertex of frame.vertices) {
-    this.parts.push(Bodies.circle(vertex.x, vertex.y, vminTOpx(0.4), { label: 'Junction' }));
+  // wheel frame
+  this.frame = [
+    // big circle
+    Bodies.circle(this.x, this.y, vminTOpx(30)),
+    // polygon
+    Bodies.polygon(this.x, this.y, this.numSeats, this.radius),
+    // small circle
+    Bodies.circle(this.x, this.y, vminTOpx(10))
+  ];
+
+  // junction points
+  this.junctions = [];
+  for (let vertex of this.frame[1].vertices /* frame[1]: polygon */) {
+    this.junctions.push(Bodies.circle(vertex.x, vertex.y, vminTOpx(0.4), { label: 'Junction' }));
   }
 
-  this.body = Body.create({ parts: this.parts });
-  World.add(engine.world, this.body);
+  this.body = Body.create({
+    parts: [...this.frame, ...this.junctions],
+    collisionFilter: { group: group }
+  });
 
-  // spokes
-  for (let part of this.parts) {
-    if (part.label === 'Junction') {
-      World.add(engine.world, Constraint.create({
-        bodyA: this.body,
-        bodyB: part,
-        render: {
-          anchors: false,
-          lineWidth: 0.1
-        }
-      }));
-    }
+  // spokes from center to junction points
+  this.spokes = [];
+  for (let junction of this.junctions) {
+    this.spokes.push(Constraint.create({
+      bodyA: this.body, // polygon's center
+      bodyB: junction,
+      render: { anchors: false, lineWidth: 0.3 }
+    }));
   }
+
+  this.seats = [];
+  this.seatLinks = [];
+  for (let junction of this.junctions) {
+    let seat = Template.seat(junction);
+    let a = seat.parts[1];
+    this.seatLinks.push(Constraint.create({
+      bodyA: seat,
+      pointA: Vector.sub(a.position, seat.position),
+      bodyB: this.body,
+      pointB: Vector.sub(junction.position, this.body.position),
+      length: 0,
+      stiffness: 1
+    }));
+    this.seats.push(seat);
+  }
+
+  this.show = function () { World.add(engine.world, [this.body, ...this.spokes, ...this.seats, ...this.seatLinks]); }
 }
 
 function Support(wheel, base) {
   this.wheel = wheel;
   this.base = base;
 
-  let render = {
+  let renderOptions = {
     lineWidth: 5,
     strokeStyle: 'rgba(255, 255, 255, 0.3)'
   };
@@ -104,32 +158,29 @@ function Support(wheel, base) {
   this.left = Constraint.create({
     pointA: { x: this.base.center - vwTOpx(10), y: this.base.elevation },
     bodyB: this.wheel.body,
-    render
+    renderOptions
   });
-
 
   this.right = Constraint.create({
     pointA: { x: this.base.center + vwTOpx(10), y: this.base.elevation },
     bodyB: this.wheel.body,
-    render
+    renderOptions
   });
 
-
-  World.add(engine.world, [this.left, this.right]);
+  this.show = function () { World.add(engine.world, [this.left, this.right]); }
 }
-
 
 const ferrisWheel = new FerrisWheel(50, 45);
-const ground = new Ground(50, 55 + pxTOvh(ferrisWheel.radius), 44, 1);
+const ground = new Ground(50, 53 + pxTOvh(ferrisWheel.radius), 60, 0.5);
 const support = new Support(ferrisWheel, ground);
 
+ground.show();
+support.show();
+ferrisWheel.show();
+
 setInterval(() => {
-  ferrisWheel.body.torque = 0.000001;
+  ferrisWheel.body.torque = 1;
 }, 100);
-
-function Human() {
-
-}
 
 
 // run the engine
@@ -138,4 +189,5 @@ Engine.run(engine);
 // run the renderer
 Render.run(render);
 
+// reload on resize
 window.onresize = () => document.location.reload();
