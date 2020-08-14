ea6deef461c6ca0b1092a7a139618abf27edda04
mountAP <ap44122@gmail.com>
2020-08-14 12:01:08 +0530

    done wheel and gondolas

--- ferris-wheel-matterJS/script.js
+++ ferris-wheel-matterJS/script.js
@@ -32,12 +32,7 @@ let render = Render.create({
 // add mouse control
 World.add(engine.world, MouseConstraint.create(engine, {
   mouse: Mouse.create(render.canvas),
-  constraint: {
-    stiffness: 1,
-    render: {
-      visible: false
-    }
-  }
+  constraint: { stiffness: 0.2, render: { visible: false } }
 }));
 
 
@@ -49,32 +44,83 @@ const pxTOvh = (x) => x / html.clientHeight * 100;
 const pxTOvw = (x) => x / html.clientWeight * 100;
 const degTOrad = (x) => x * Math.PI / 180;
 
-// objects
+// for collision filter
+const wheelGroup = Body.nextGroup(true);
 
-// categories
-const group = Body.nextGroup(true);
 
-const Template = {
-  seat: function (junction) {
-    this.x = junction.position.x;
-    this.y = junction.position.y;
-    const r = 2.5; // traingle radius
-    const h = r + r / 2; // triangle height
-    const l = 4; // square length
-    const b = r * Math.sqrt(3);// square bredth
+const CustomComposites = {
 
-    let cap = Bodies.polygon(this.x, this.y, 3, vminTOpx(r), { angle: degTOrad(90) });
-    let basket = Bodies.rectangle(this.x, this.y + vminTOpx(h - r + l / 2), vminTOpx(b), vminTOpx(l));
-    let a = Bodies.circle(this.x, cap.position.y, 0.4);
-    return Body.create({
-      parts: [cap, basket, a],
-      collisionFilter: { group: group },
-      restitution: 0,
+  gondola: function (x, y, wheelFrame) {
+    let gondolaComposite = Composite.create();
+
+    let r = 2.5; // traingle radius
+    let h = r + r / 2; // triangle height
+    let cap = Bodies.polygon(x, y, 3, vminTOpx(r), { angle: degTOrad(90), chamfer: { radius: [0, vminTOpx(1), 0] } }); // triangle
+
+    let l = 4; // square length
+    let b = r * Math.sqrt(3);// square bredth
+    let basket = Bodies.rectangle(x, y + vminTOpx(h - r + l / 2), vminTOpx(b), vminTOpx(l));
+
+    let jointPos = (cap.position.y + cap.vertices[1].y) / 2;
+    let joint = Bodies.circle(x, jointPos, vminTOpx(0.5));
+
+    let body = Body.create({
+      parts: [cap, basket, joint],
+      collisionFilter: { group: wheelGroup },
       density: 0.01,
-      frictionAir: 0.05
     });
+
+    let link = Constraint.create({
+      bodyA: body,
+      pointA: Vector.sub(joint.position, body.position),
+      bodyB: wheelFrame,
+      pointB: Vector.sub({ x: x, y: y }, wheelFrame.position),
+      length: 0,
+      stiffness: 1,
+      render: { lineWidth: 0.7 }
+    });
+
+    Composite.add(gondolaComposite, [body, link]);
+    return gondolaComposite;
   }
+
 };
+
+function FerrisWheel(x_vw, y_vh) {
+  this.x = vwTOpx(x_vw);
+  this.y = vhTOpx(y_vh);
+  this.radius = vminTOpx(40); // radius  } hard
+  this.numSeats = 14; // number of seats } coded
+
+  // wheel frame
+  this.frame = Body.create({
+    parts: [
+      Bodies.circle(this.x, this.y, vminTOpx(30)), // big circle
+      Bodies.polygon(this.x, this.y, this.numSeats, this.radius), // polygon
+      Bodies.circle(this.x, this.y, vminTOpx(10)) // small circle
+    ],
+    collisionFilter: { group: wheelGroup }
+  });
+
+  // spokes from center to polgon vertices
+  this.spokes = [];
+  for (let vertex of this.frame.vertices) {
+    this.spokes.push(Constraint.create({
+      bodyA: this.frame,
+      bodyB: this.frame,
+      pointB: Vector.sub({ x: vertex.x, y: vertex.y }, this.frame.position), // polygon vertex
+      render: { anchors: false, lineWidth: 0.3 }
+    }));
+  }
+
+  // gondolas
+  this.gondolas = [];
+  for (let vertex of this.frame.vertices) {
+    this.gondolas.push(CustomComposites.gondola(vertex.x, vertex.y, this.frame));
+  }
+
+  this.show = function () { World.add(engine.world, [this.frame, ...this.spokes, ...this.gondolas]); }
+}
 
 function Ground(center_vw, elevation_vh, span_vw, thickness_vh) {
   this.center = vwTOpx(center_vw);
@@ -86,68 +132,8 @@ function Ground(center_vw, elevation_vh, span_vw, thickness_vh) {
   this.show = function () { World.add(engine.world, this.body); }
 }
 
-function FerrisWheel(x_vw, y_vh) {
-  this.x = vwTOpx(x_vw);
-  this.y = vhTOpx(y_vh);
-  this.radius = vminTOpx(40);
-  this.numSeats = 24;
-
-  // this.body = Composite.create();
-
-  // Composite.add(this.body, )
-
-  // wheel frame
-  this.frame = [
-    // big circle
-    Bodies.circle(this.x, this.y, vminTOpx(30)),
-    // polygon
-    Bodies.polygon(this.x, this.y, this.numSeats, this.radius),
-    // small circle
-    Bodies.circle(this.x, this.y, vminTOpx(10))
-  ];
-
-  // junction points
-  this.junctions = [];
-  for (let vertex of this.frame[1].vertices /* frame[1]: polygon */) {
-    this.junctions.push(Bodies.circle(vertex.x, vertex.y, vminTOpx(0.4), { label: 'Junction' }));
-  }
-
-  this.body = Body.create({
-    parts: [...this.frame, ...this.junctions],
-    collisionFilter: { group: group }
-  });
-
-  // spokes from center to junction points
-  this.spokes = [];
-  for (let junction of this.junctions) {
-    this.spokes.push(Constraint.create({
-      bodyA: this.body, // polygon's center
-      bodyB: junction,
-      render: { anchors: false, lineWidth: 0.3 }
-    }));
-  }
-
-  this.seats = [];
-  this.seatLinks = [];
-  for (let junction of this.junctions) {
-    let seat = Template.seat(junction);
-    let a = seat.parts[1];
-    this.seatLinks.push(Constraint.create({
-      bodyA: seat,
-      pointA: Vector.sub(a.position, seat.position),
-      bodyB: this.body,
-      pointB: Vector.sub(junction.position, this.body.position),
-      length: 0,
-      stiffness: 1
-    }));
-    this.seats.push(seat);
-  }
-
-  this.show = function () { World.add(engine.world, [this.body, ...this.spokes, ...this.seats, ...this.seatLinks]); }
-}
-
-function Support(wheel, base) {
-  this.wheel = wheel;
+function Support(body, base) {
+  this.body = body;
   this.base = base;
 
   let renderOptions = {
@@ -157,13 +143,13 @@ function Support(wheel, base) {
 
   this.left = Constraint.create({
     pointA: { x: this.base.center - vwTOpx(10), y: this.base.elevation },
-    bodyB: this.wheel.body,
+    bodyB: this.body,
     renderOptions
   });
 
   this.right = Constraint.create({
     pointA: { x: this.base.center + vwTOpx(10), y: this.base.elevation },
-    bodyB: this.wheel.body,
+    bodyB: this.body,
     renderOptions
   });
 
@@ -172,15 +158,13 @@ function Support(wheel, base) {
 
 const ferrisWheel = new FerrisWheel(50, 45);
 const ground = new Ground(50, 53 + pxTOvh(ferrisWheel.radius), 60, 0.5);
-const support = new Support(ferrisWheel, ground);
+const support = new Support(ferrisWheel.frame, ground);
 
+ferrisWheel.show();
 ground.show();
 support.show();
-ferrisWheel.show();
 
-setInterval(() => {
-  ferrisWheel.body.torque = 1;
-}, 100);
+setInterval(() => { ferrisWheel.frame.torque = 3; }, 100);
 
 
 // run the engine
