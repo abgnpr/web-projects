75d8fcfbd40395f30dd85b48fc6b81e1c45e7814
mountAP <ap44122@gmail.com>
2020-08-16 18:34:47 +0530

    spin settings
    - reset angular velocity on spin off

--- ferris-wheel-matterJS/controls.js
+++ ferris-wheel-matterJS/controls.js
@@ -2,7 +2,7 @@ const lock = document.querySelector('.lock');
 const lockedIcon = './images/locked.png';
 const unockedIcon = './images/unlocked.png';
 const spinToggle = document.querySelector('.spin-toggle');
-const speedControl = document.querySelector('.speed-slider');
+const slider = document.querySelector('.slider');
 
 // axle lock (default: locked)
 let axleLocked = ferrisWheel.axle.isStatic;
@@ -24,21 +24,23 @@ lock.addEventListener('click', function (e) {
 });
 
 
+// range (0.000 - 0.100)
+let angularVelocity = 0.002;
+// slider is also defaulted to 2 in html (range 0 - 100)
+
 // spin controls
 let spin = {
-
   on: true, // default: spinning
-
   toggleOff: function () {
     this.on = false;
     spinToggle.classList.remove('spinning');
+    angularVelocity = 0.002;
+    slider.value = 2;
   },
-
   toggleOn: function () {
     this.on = true;
     spinToggle.classList.add('spinning');
   }
-
 };
 
 // turns spin on or off when the toggle icon is clicked
@@ -46,21 +48,19 @@ spinToggle.addEventListener('click', function (e) {
   (spin.on) ? spin.toggleOff() : spin.toggleOn();
 });
 
-
+// mouse activity flag
 let mouseInUse = false; // true when mouse events are active
 Events.on(mouseConstraint, "mousedown", () => { mouseInUse = true; });
 Events.on(mouseConstraint, "mouseup", () => { mouseInUse = false });
 
-// range (0.000 - 0.100)
-let angularVelocity = speedControl.value / 1000;
-
-speedControl.addEventListener('input', function (e) {
-  angularVelocity = speedControl.value / 1000;
-});
-
-// Angular Velocity settings
+// angular velocity settings
 Events.on(engine, 'beforeUpdate', function (e) {
   if (!mouseInUse && spin.on && ferrisWheel.frame.angularVelocity <= angularVelocity) {
     Body.setAngularVelocity(ferrisWheel.frame, angularVelocity);
   }
+});
+
+// update angular velocity according to the value of slider
+slider.addEventListener('input', function (e) {
+  angularVelocity = slider.value / 1000;
 });
