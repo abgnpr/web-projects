d9d0cbe2fd4a0bb79c1f06fac068bc1ab497c679
mountAP <ap44122@gmail.com>
2021-02-08 23:19:31 +0530

    navbar shadow

--- srk-ent/script.dev.js
+++ srk-ent/script.dev.js
@@ -25,8 +25,10 @@ container.addEventListener("scroll", function () {
   if (current_pos < checkpoint) {
     navbar.style.top = "".concat((1 - ratio) * -navbar_orig_height, "px");
     logo.style.opacity = Math.pow(1 - ratio, 4);
+    navbar.classList.remove('shadow');
   } else {
     navbar.style.top = 0;
+    navbar.classList.add('shadow');
     logo.style.opacity = 0;
   }
 });
--- srk-ent/src/script.js
+++ srk-ent/src/script.js
@@ -21,8 +21,10 @@ container.addEventListener("scroll", () => {
   if (current_pos < checkpoint) {
     navbar.style.top = `${(1 - ratio) * -navbar_orig_height}px`;
     logo.style.opacity = Math.pow(1 - ratio, 4);
+    navbar.classList.remove('shadow')
   } else {
     navbar.style.top = 0;
+    navbar.classList.add('shadow')
     logo.style.opacity = 0;
   }
 });
