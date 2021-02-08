61e17c8f5e050093fd8926c2be80858ef69b94b8
mountAP <ap44122@gmail.com>
2021-02-08 22:37:29 +0530

    building srk-ent 8 Feb 2021

--- srk-ent/script.dev.js
+++ srk-ent/script.dev.js
@@ -1,20 +1,32 @@
 "use strict";
 
+// let vwInPx = width => (width * window.innerWidth) / 100;
+var vhInPx = function vhInPx(height) {
+  return height * window.innerHeight / 100;
+};
+
 var container = document.querySelector(".container");
 var anchor = document.querySelector(".anchor");
 var navbar = document.querySelector(".navbar");
-var observer = new IntersectionObserver(function (entries) {
-  if (entries[entries.length - 1].intersectionRatio > 0) {
-    navbar.classList.remove("show"); // hide
+var logo = document.querySelector(".logo");
+var logo_container = document.querySelector(".logo-container");
+var navbar_orig_height;
+var checkpoint;
 
-    console.log("hide");
+window.onload = window.onresize = function () {
+  checkpoint = logo_container.scrollHeight - navbar.scrollHeight - 1;
+  navbar_orig_height = navbar.clientHeight;
+};
+
+container.addEventListener("scroll", function () {
+  var current_pos = container.scrollTop;
+  var ratio = current_pos / checkpoint;
+
+  if (current_pos < checkpoint) {
+    navbar.style.top = "".concat((1 - ratio) * -navbar_orig_height, "px");
+    logo.style.opacity = Math.pow(1 - ratio, 4);
   } else {
-    navbar.classList.add("show");
-    console.log("show");
+    navbar.style.top = 0;
+    logo.style.opacity = 0;
   }
-}, {
-  root: null,
-  rootMargin: "0px",
-  threshold: 1.0
 });
-observer.observe(anchor);
--- srk-ent/src/script.js
+++ srk-ent/src/script.js
@@ -1,22 +1,28 @@
+// let vwInPx = width => (width * window.innerWidth) / 100;
+let vhInPx = height => (height * window.innerHeight) / 100;
+
 const container = document.querySelector(".container");
 const anchor = document.querySelector(".anchor");
 const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo");
+const logo_container = document.querySelector(".logo-container");
 
-const observer = new IntersectionObserver(
-  entries => {
-    if (entries[entries.length - 1].intersectionRatio > 0) {
-      navbar.classList.remove("show"); // hide
-      console.log("hide");
-    } else {
-      navbar.classList.add("show");
-      console.log("show");
-    }
-  },
-  {
-    root: null,
-    rootMargin: "0px",
-    threshold: 1.0,
+let navbar_orig_height;
+let checkpoint;
+
+window.onload = window.onresize = () => {
+  checkpoint = logo_container.scrollHeight - navbar.scrollHeight - 1;
+  navbar_orig_height = navbar.clientHeight;
+};
+
+container.addEventListener("scroll", () => {
+  let current_pos = container.scrollTop;
+  let ratio = current_pos / checkpoint;
+  if (current_pos < checkpoint) {
+    navbar.style.top = `${(1 - ratio) * -navbar_orig_height}px`;
+    logo.style.opacity = Math.pow(1 - ratio, 4);
+  } else {
+    navbar.style.top = 0;
+    logo.style.opacity = 0;
   }
-);
-
-observer.observe(anchor);
+});
